1def53a095000c2b1d4ae798b344ebc539e36eb3
marcogalter <marcogalter686@gmail.com>
2024-05-06 14:51:43 -0300

    estruturas

--- estruturas-condicionais/src/Boletim.java
+++ estruturas-condicionais/src/Boletim.java
@@ -7,12 +7,12 @@ public class Boletim {
         System.out.print("Digite a nota da prova: ");
         double nota = scanner.nextDouble();
 
-        //if (nota >= 7)
-        //System.out.println("Aprovado!"); 
-        //else if(nota >= 5 && nota < 7)
-        //System.out.println("Recuperação!");
-        //else
-            //System.out.println("Reprovado!");
+        /*if (nota >= 7)
+        System.out.println("Aprovado!"); 
+        else if(nota >= 5 && nota < 7)
+        System.out.println("Recuperação!");
+        else
+            System.out.println("Reprovado!");*/
         
         String resultado = nota >= 7 ? "Aprovado!" : nota >= 5 && nota < 7 ? "Recuperação!" : "Reprovado!";
         System.out.println(resultado);
